f05b6d7853406d67723054f26b33b46bf59e427a
UltraOnGit <149175757+UltraOnGit@users.noreply.github.com>
2026-05-06 12:02:28 -0400

    Add Footer component with social media message

---
+++ src/assets/layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = ({ text }) => {
+    return (
+        <footer className="footer">
+            <p>{text}: Follow our development on social media soon!</p>
+        </footer>
+    );
+}
+
+export default Footer;

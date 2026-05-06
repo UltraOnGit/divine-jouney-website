b63a5c0de792c823ff6ae257e144a791be241bd3
UltraOnGit <149175757+UltraOnGit@users.noreply.github.com>
2026-05-06 12:20:20 -0400

    Add files via upload

---
+++ src/assets/pages/Weapons.jsx
@@ -0,0 +1,12 @@
+import './Weapons.css'
+import React from "react";
+
+const Weapons = () => {
+     return (
+        <div className="mainBlock">
+            <h1>Weapons 123</h1>
+        </div>
+    );
+
+}
+export default Weapons

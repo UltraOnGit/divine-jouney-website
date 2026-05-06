59191e15fb98cdd5dad2186e4e5a82f36a2de487
UltraOnGit <149175757+UltraOnGit@users.noreply.github.com>
2026-05-06 12:08:07 -0400

    Create Collectibles component with content and image
    Replaced Vaporsteam component with Collectibles component that displays information about collectibles and includes an image.

--- src/assets/pages/Collectibles.jsx
+++ src/assets/pages/Collectibles.jsx
@@ -1,12 +1,25 @@
 import './Collectibles.css'
 import React from "react";
 
-const Vaporsteam = () => {
-
+const Collectibles = () => {
     return (
-        <h1>Testing testing 123</h1>
-    )
+        <div className="mainBlock">
+            <h1>Collectibles</h1>
+            <p className="glow">
+            Seek valuable resources on your journey!
+            </p>
+
+             <div className="parent">
+              <div className="div1"><img src="/src/assets/images/AscendancyOrb.png" alt="Ascendancy Orb"/></div>
+              <div className="div2"><h3 className="gradient-text">Ascendancy Orb</h3></div>
+              <div className="div3">Ascendancy orbs are the main way to traverse worlds in Divine Journey! After
+                freeing each celestial, they will give you an Ascendancy Orb as a token of trust, allowing you
+                to explore the galaxy further and find more secrets!
+              </div>
+          </div>
+        </div>
+    );
 
 }
 
-export default Vaporsteam
+export default Collectibles

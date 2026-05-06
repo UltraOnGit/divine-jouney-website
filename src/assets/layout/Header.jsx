aca90b35202bc26f9356320fe859cc1c418a8cce
UltraOnGit <149175757+UltraOnGit@users.noreply.github.com>
2026-05-06 12:03:59 -0400

    Add Header component with basic structure

---
+++ src/assets/layout/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="headerImage">
+            Divine Journey
+        </header>
+    );
+
+}
+
+export default Header
